5a07a95624516aa4bb5f478aaa7b1eac103d48b6
oeway <oeway@users.noreply.github.com>
2020-06-06 21:18:59 +0000

    Deploy to GitHub Pages 6f88228cca2225f1f73e7a2bec2bb3058913093d

---
+++ precache-manifest.192bc040a0672272067107f241c59e90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b940021f70bbbaeb208",
+    "url": "/css/app.ceb752fe.css"
+  },
+  {
+    "revision": "411ad82ff988691a8d75",
+    "url": "/css/chunk-vendors.310ad090.css"
+  },
+  {
+    "revision": "51b61a281ba819835d770760b024e04f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "657a27fb13b0140865eb",
+    "url": "/js/about.6e156aac.js"
+  },
+  {
+    "revision": "4b940021f70bbbaeb208",
+    "url": "/js/app.80922347.js"
+  },
+  {
+    "revision": "411ad82ff988691a8d75",
+    "url": "/js/chunk-vendors.06a56008.js"
+  },
+  {
+    "revision": "b2617957eaf2e8ca8de62e873e5464b2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
