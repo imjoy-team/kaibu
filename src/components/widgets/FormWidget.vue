82b55254fb47a3e84ca1149689373915a67428a1
Wei Ouyang <oeway007@gmail.com>
2021-01-26 22:59:26 +0100

    Add form widget

--- src/components/widgets/FormWidget.vue
+++ src/components/widgets/FormWidget.vue
@@ -1,11 +1,48 @@
 <template>
-  <div class="form-widgett"></div>
+  <div class="form-widgett">
+    <form-json
+      :btnReset="{ value: 'Reset' }"
+      :btnSubmit="{ value: 'Submit' }"
+      :camelizePayloadKeys="true"
+      :formFields="jsonFields"
+      formName="userProfil"
+    >
+    </form-json>
+  </div>
 </template>
 
 <script>
+import "bulma/css/bulma.min.css";
+import "vue-form-json/dist/vue-form-json.css";
+import formJson from "vue-form-json";
+
+const jsonFields = [
+  [
+    {
+      label: "First Name",
+      value: "fir",
+      rules: {
+        min: 4,
+        max: 20
+      }
+    },
+    {
+      label: "Last Name",
+      placeholder: "Last Name placeholder",
+      rules: {
+        min: 3
+      }
+    }
+  ],
+  {
+    html:
+      "<div class='box'><article class='media'><div class='media-left'><figure class='image is-64x64'><img src='https://bulma.io/images/placeholders/64x64.png' alt='Image'></figure></div><div class='media-content'><div class='content'><p><strong class='has-text-info'>Info</strong><br>You can also pass html like this box</p></div>"
+  }
+];
 export default {
   name: "form-widget",
   type: "form",
+  components: { "form-json": formJson },
   props: {
     config: {
       type: Object,
@@ -17,7 +54,11 @@ export default {
   data() {
     return {};
   },
+  computed: {
+    jsonFields: () => jsonFields
+  },
   mounted() {
+    this.$root.$on("formSubmitted", values => alert(JSON.stringify(values)));
     if (this.config._resolve) {
       this.config._resolve({});
     }
